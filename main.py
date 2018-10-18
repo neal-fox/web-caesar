870926cebae6a7d61b03b865f43da9a165d54829
Neal Fox <nealfox1@gmail.com>
2018-10-18 11:45:21 -0500

    add output encrypted text to textarea of original form

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -29,6 +29,7 @@ form = """
             <label for="rot">Rotate by:</label>
             <input type="text" name="rot" value="0"/>
             <textarea name="text" class="textarea">
+            {0}
             </textarea>
             <input type="submit" value="Submit Query"/>
         </form>
@@ -38,16 +39,14 @@ form = """
 
 @app.route("/")
 def index():
-    return form
+    return form.format('')
 
 @app.route("/", methods=["POST"])
 def encrypt():
     rot = int(request.form['rot'])
     text = request.form['text']
     encrypted = rotate_string(text, rot)
-    output = """
-    <h1>{0}</h1>
-    """.format(encrypted)
-    return output
-    
+
+    return form.format(encrypted)
+
 app.run()
